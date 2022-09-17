c04dca35c0dc640f448c5669487f61ba487441ce
sandeep <sandeepsomavarapu535@gmail.com>
2022-09-17 12:50:55 +0530

    update from sts

--- SpringBootEmployeeCrudUsingjpaHIbernate/src/main/java/com/rps/demo/controller/EmployeeController.java
+++ SpringBootEmployeeCrudUsingjpaHIbernate/src/main/java/com/rps/demo/controller/EmployeeController.java
@@ -33,6 +33,7 @@ public class EmployeeController {
 	@PostMapping("/addemployee") // http://localhost:7878/employee/addemployee
 	public String addEmployee(@Valid@RequestBody Employee emp) {
 		System.out.println("one line added");
+		System.out.println("two lines added");
 		return service.employeeCreation(emp);
 	}
 
